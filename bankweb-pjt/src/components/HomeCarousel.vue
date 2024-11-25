5c1db0bf489c1f6255cba403aef30e7c451b9513
sungwon <trevivom76@naver.com>
2024-11-25 13:45:47 +0900

    [feat] 안쓰이는 코드 삭제

--- bankweb-pjt/src/components/HomeCarousel.vue
+++ bankweb-pjt/src/components/HomeCarousel.vue
@@ -1,22 +1,5 @@
-<template>
-  <!-- 레이아웃까지 구성 -->
-  <div class="container">
-    <v-carousel show-arrows="hover">
-      <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover></v-carousel-item>
+<template></template>
 
-      <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
+<script setup></script>
 
-      <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
-    </v-carousel>
-  </div>
-</template>
-
-<script setup>
-</script>
-
-<style scoped>
-.container {
-  border-radius: 20px;
-  overflow: hidden;
-}
-</style>
+<style scoped></style>
